b5296b64f2e393470755d19306f4a6254c0e6731
vitaliy2005 <svitala500@gmail.com>
2024-08-21 13:25:43 +0700

    fix

---
+++ module5/1/kernel.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("WRG");
+MODULE_AUTHOR("WARG@gmail.com");
+MODULE_DESCRIPTION("Hello World Kernel Module");
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello, World!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Goodbye, World!\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
